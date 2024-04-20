e1e58eeb7e9b07e5a8e212caa143280d0a94ec4b
Jakub Mitkowski <105969878+lel05@users.noreply.github.com>
2024-04-20 20:05:04 +0200

    added offer to mainpage

--- main/index.php
+++ main/index.php
@@ -19,13 +19,29 @@
     ?>
   </header>
 
-  <?php
+  <section style="background-color: red; height: 200px;">
+    <!-- wyszukiwarka -->
+  </section>
 
+  <section class="position-absolute top-50 start-50 translate-middle">
+    <?php
 
-
-
-
-  ?>
+    $sql = "SELECT * FROM offer";
+    $result = $conn->query($sql);
+    while ($row = $result->fetch_assoc()) {
+      echo '<div style="width: 350px; height: 200px; padding: 5px;" class="border rounded shadow-sm float-start m-3">';
+        echo '<div>';
+          echo '<img src="../Images/logos/logoRounded.ico" alt="company logo" style="width: 70px;" class="float-start me-1">';
+          echo '<div>';
+            echo '<label>'.$row['profession_name'].'</label><br>';
+            echo '<label>'.$row['duties'].'</label><br>';
+            echo '<label>'.$row['salary'].'</label>';
+          echo '</div>';
+        echo '</div>';
+      echo '</div>';
+    }
+    ?>
+  </section>
 
 </body>
 
